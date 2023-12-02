d5b2670d71d6c061bb605e6b50c3410d14ede68d
Gregor Majcen <majcn.m@gmail.com>
2023-12-02 07:56:20 +0100

    2nd day

---
+++ src/bin/02.rs
@@ -0,0 +1,96 @@
+advent_of_code::solution!(2);
+
+#[derive(Debug)]
+struct Game {
+    id: u32,
+    sets: Vec<Set>,
+}
+
+#[derive(Debug, Default)]
+struct Set {
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+impl From<&str> for Set {
+    fn from(item: &str) -> Self {
+        let mut result = Set::default();
+
+        item.split(", ")
+            .filter_map(|x| x.split_once(' '))
+            .for_each(|(n, color)| match color {
+                "red" => result.red = n.parse().unwrap(),
+                "green" => result.green = n.parse().unwrap(),
+                "blue" => result.blue = n.parse().unwrap(),
+                _ => unreachable!(),
+            });
+
+        result
+    }
+}
+
+impl From<&str> for Game {
+    fn from(item: &str) -> Self {
+        let (game_id, game_data) = item.split_once(": ").unwrap();
+
+        let id = game_id[5..].parse().unwrap();
+        let sets = game_data.split("; ").map(Set::from).collect();
+
+        Game { id, sets }
+    }
+}
+
+fn parse_data(input: &str) -> Vec<Game> {
+    input.lines().map(Game::from).collect()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let data = parse_data(input);
+
+    let result = data
+        .into_iter()
+        .filter(|game| {
+            !game
+                .sets
+                .iter()
+                .any(|set| set.red > 12 || set.green > 13 || set.blue > 14)
+        })
+        .map(|game| game.id)
+        .sum();
+
+    Some(result)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let data = parse_data(input);
+
+    let result = data
+        .into_iter()
+        .map(|game| Set {
+            red: game.sets.iter().map(|x| x.red).max().unwrap(),
+            green: game.sets.iter().map(|x| x.green).max().unwrap(),
+            blue: game.sets.iter().map(|x| x.blue).max().unwrap(),
+        })
+        .map(|set| set.red * set.green * set.blue)
+        .sum();
+
+    Some(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(8));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2286));
+    }
+}
