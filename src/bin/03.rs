8a35df02853ca5b510648a07009aa856b477e1db
Gregor Majcen <majcn.m@gmail.com>
2023-12-04 21:41:12 +0100

    simplify with regex

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,9 +1,11 @@
 advent_of_code::solution!(3);
 
+use advent_of_code::util::point::Point;
+
+use regex::Regex;
+
 use std::collections::HashMap;
 use std::ops::Add;
-
-use advent_of_code::util::point::Point;
 
 #[derive(Debug, Default)]
 struct Number {
@@ -18,21 +20,16 @@ fn parse_data(input: &str) -> (Vec<Number>, Symbols) {
     let mut numbers = vec![];
     let mut symbols = Symbols::new();
 
+    let re_numbers = Regex::new(r"\d+").unwrap();
+
     for (y, line) in input.lines().enumerate() {
-        let mut current_number = Number::default();
-        for (x, v) in line.chars().enumerate() {
-            if v.is_ascii_digit() {
-                current_number.value = current_number.value * 10 + v.to_digit(10).unwrap();
-                current_number
-                    .locations
-                    .push(Point::new(x as i32, y as i32))
-            } else if current_number.value > 0 {
-                numbers.push(current_number);
-                current_number = Number::default();
-            }
-        }
-        if current_number.value > 0 {
-            numbers.push(current_number);
+        for m in re_numbers.find_iter(line) {
+            let value = m.as_str().parse().unwrap();
+            let locations = (m.start()..m.end())
+                .map(|x| Point::new(x as i32, y as i32))
+                .collect();
+
+            numbers.push(Number { value, locations });
         }
     }
 
@@ -47,10 +44,8 @@ fn parse_data(input: &str) -> (Vec<Number>, Symbols) {
     (numbers, symbols)
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let (numbers, symbols) = parse_data(input);
-
-    let neighbors = [
+fn find_symbol_location(number: &Number, symbols: &Symbols) -> Option<Point> {
+    const NEIGHBORS: [Point; 8] = [
         Point::new(-1, -1),
         Point::new(-1, 0),
         Point::new(-1, 1),
@@ -61,21 +56,27 @@ pub fn part_one(input: &str) -> Option<u32> {
         Point::new(1, 1),
     ];
 
-    let is_valid = |number: &Number| {
-        for number_location in number.locations.iter() {
-            for n in neighbors {
-                let new_location = number_location.add(n);
+    for number_location in number.locations.iter() {
+        for n in NEIGHBORS {
+            let new_location = number_location.add(n);
 
-                if symbols.contains_key(&new_location) {
-                    return true;
-                }
+            if symbols.contains_key(&new_location) {
+                return Some(new_location);
             }
         }
+    }
 
-        false
-    };
+    None
+}
 
-    let result = numbers.into_iter().filter(is_valid).map(|x| x.value).sum();
+pub fn part_one(input: &str) -> Option<u32> {
+    let (numbers, symbols) = parse_data(input);
+
+    let result = numbers
+        .into_iter()
+        .filter(|x| find_symbol_location(x, &symbols).is_some())
+        .map(|x| x.value)
+        .sum();
 
     Some(result)
 }
@@ -83,38 +84,16 @@ pub fn part_one(input: &str) -> Option<u32> {
 pub fn part_two(input: &str) -> Option<u32> {
     let (numbers, symbols) = parse_data(input);
 
-    let neighbors = [
-        Point::new(-1, -1),
-        Point::new(-1, 0),
-        Point::new(-1, 1),
-        Point::new(0, -1),
-        Point::new(0, 1),
-        Point::new(1, -1),
-        Point::new(1, 0),
-        Point::new(1, 1),
-    ];
-
-    let find_symbol = |number: &Number| {
-        for number_location in number.locations.iter() {
-            for n in neighbors {
-                let new_location = number_location.add(n);
-
-                if let Some(x) = symbols.get(&new_location) {
-                    if x == &'*' {
-                        return Some(new_location);
-                    }
-                }
-            }
-        }
-
-        None
-    };
-
     let mut symbol_values: HashMap<Point, Vec<u32>> = HashMap::new();
 
     for number in numbers {
-        if let Some(symbol) = find_symbol(&number) {
-            symbol_values.entry(symbol).or_default().push(number.value);
+        if let Some(symbol_location) = find_symbol_location(&number, &symbols) {
+            if symbols.get(&symbol_location).unwrap() == &'*' {
+                symbol_values
+                    .entry(symbol_location)
+                    .or_default()
+                    .push(number.value);
+            }
         }
     }
 
