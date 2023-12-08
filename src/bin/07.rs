91b68ade7314eb68a532410d94ff76bc9fa9f1b4
Gregor Majcen <majcn.m@gmail.com>
2023-12-08 16:44:02 +0100

    7th day

---
+++ src/bin/07.rs
@@ -0,0 +1,160 @@
+advent_of_code::solution!(7);
+
+use std::collections::HashMap;
+
+struct Hand {
+    cards: Vec<char>,
+    bid: u32,
+}
+
+#[derive(PartialEq, PartialOrd)]
+enum HandPower {
+    None,
+    Pair,
+    TwoPairs,
+    ThreeOfKind,
+    FullHouse,
+    FourOfKind,
+    FiveOfKind,
+}
+
+struct HandState {
+    bid: u32,
+    power: HandPower,
+    values: Vec<u8>,
+}
+
+fn parse_data(input: &str) -> Vec<Hand> {
+    input
+        .lines()
+        .map(|line| {
+            let cards = line[..5].chars().collect();
+            let bid = line[6..].parse().unwrap();
+            Hand { cards, bid }
+        })
+        .collect()
+}
+
+fn calculate_power(hand: &Hand, joker_mode: bool) -> HandPower {
+    let mut counter = HashMap::with_capacity(hand.cards.len());
+
+    for x in &hand.cards {
+        *counter.entry(x).or_default() += 1;
+    }
+
+    let jokers = if joker_mode {
+        counter.remove(&'J').unwrap_or(0)
+    } else {
+        0
+    };
+
+    let best_value = counter.values().max().unwrap_or(&0) + jokers;
+
+    if best_value == 5 {
+        return HandPower::FiveOfKind;
+    }
+    if best_value == 4 {
+        return HandPower::FourOfKind;
+    }
+    if best_value == 3 {
+        if counter.values().count() == 2 {
+            return HandPower::FullHouse;
+        } else {
+            return HandPower::ThreeOfKind;
+        }
+    }
+    if best_value == 2 {
+        if counter.values().count() == 3 {
+            return HandPower::TwoPairs;
+        } else {
+            return HandPower::Pair;
+        }
+    }
+
+    HandPower::None
+}
+
+fn part_x(hands: &[Hand], joker_mode: bool) -> u32 {
+    let mut state = hands
+        .iter()
+        .map(|hand| {
+            let bid = hand.bid;
+            let power = calculate_power(hand, joker_mode);
+            let values = hand
+                .cards
+                .iter()
+                .map(|x| match (x, joker_mode) {
+                    ('J', true) => 1,
+                    ('2', _) => 2,
+                    ('3', _) => 3,
+                    ('4', _) => 4,
+                    ('5', _) => 5,
+                    ('6', _) => 6,
+                    ('7', _) => 7,
+                    ('8', _) => 8,
+                    ('9', _) => 9,
+                    ('T', _) => 10,
+                    ('J', false) => 11,
+                    ('Q', _) => 12,
+                    ('K', _) => 13,
+                    ('A', _) => 14,
+                    _ => unreachable!(),
+                })
+                .collect();
+
+            HandState { bid, power, values }
+        })
+        .collect::<Vec<_>>();
+
+    state.sort_unstable_by(|a, b| {
+        if a.power == b.power {
+            let mut values_iter = a.values.iter().zip(b.values.iter());
+
+            return values_iter
+                .find(|(av, bv)| av != bv)
+                .map(|(av, bv)| av.cmp(bv))
+                .unwrap();
+        }
+
+        a.power.partial_cmp(&b.power).unwrap()
+    });
+
+    state
+        .into_iter()
+        .enumerate()
+        .map(|(i, s)| (i as u32 + 1) * s.bid)
+        .sum()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let hands = parse_data(input);
+
+    let result = part_x(&hands, false);
+
+    Some(result)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let hands = parse_data(input);
+
+    let result = part_x(&hands, true);
+
+    Some(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(6440));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(5905));
+    }
+}
