eb62be7eb2ef9e97c4c8fb8328a3d41be2d5a270
Gregor Majcen <majcn.m@gmail.com>
2023-12-08 16:45:26 +0100

    6th day refactor

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,30 +1,36 @@
 advent_of_code::solution!(6);
 
-fn parse_data(input: &str) -> (Vec<u64>, Vec<u64>) {
+struct Race {
+    time: u64,
+    distance: u64,
+}
+
+fn parse_data(input: &str) -> Vec<Race> {
     let (time, distance) = input.split_once('\n').unwrap();
+
     let time = time[5..]
         .split_ascii_whitespace()
-        .map(|x| x.parse().unwrap())
-        .collect();
+        .map(|x| x.parse().unwrap());
 
     let distance = distance[9..]
         .split_ascii_whitespace()
-        .map(|x| x.parse().unwrap())
-        .collect();
+        .map(|x| x.parse().unwrap());
 
-    (time, distance)
+    time.zip(distance)
+        .map(|(time, distance)| Race { time, distance })
+        .collect()
 }
 
-fn part_x(time: u64, distance: u64) -> u64 {
+fn part_x(race: Race) -> u64 {
     let mut a = 0;
-    let mut b = time / 2;
+    let mut b = race.time / 2;
     loop {
         let c = (a + b) / 2;
-        if c * (time - c) > distance && (c - 1) * (time - c + 1) <= distance {
+        if c * (race.time - c) > race.distance && (c - 1) * (race.time - c + 1) <= race.distance {
             break c;
         }
 
-        if c * (time - c) <= distance {
+        if c * (race.time - c) <= race.distance {
             a = c;
         } else {
             b = c;
@@ -33,31 +39,37 @@ fn part_x(time: u64, distance: u64) -> u64 {
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let (time, distance) = parse_data(input);
+    let races = parse_data(input);
 
-    let mut result = 1;
-    for i in 0..time.len() {
-        let best_time = part_x(time[i], distance[i]);
-        result *= time[i] - best_time - best_time + 1;
-    }
+    let result = races
+        .into_iter()
+        .map(|race| (race.time, part_x(race)))
+        .map(|(time, best_time)| time - best_time - best_time + 1)
+        .product::<u64>();
 
     Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let (time, distance) = parse_data(input);
-    let time = time
-        .into_iter()
-        .fold(String::new(), |acc, x| acc + &x.to_string())
-        .parse()
-        .unwrap();
-    let distance = distance
-        .into_iter()
+    let races = parse_data(input);
+
+    let time = races
+        .iter()
+        .map(|race| race.time)
         .fold(String::new(), |acc, x| acc + &x.to_string())
         .parse()
         .unwrap();
 
-    let best_time = part_x(time, distance);
+    let distance = races
+        .iter()
+        .map(|race| race.distance)
+        .fold(String::new(), |acc, x| acc + &x.to_string())
+        .parse()
+        .unwrap();
+
+    let race = Race { time, distance };
+
+    let best_time = part_x(race);
     let result = time - best_time - best_time + 1;
 
     Some(result)
