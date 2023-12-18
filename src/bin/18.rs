e52054f2e40e7735332dbc80f7a17bccaa1ad015
Gregor Majcen <majcn.m@gmail.com>
2023-12-18 16:55:55 +0100

    add tests

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -110,12 +110,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(62));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(952408144115));
     }
 }
