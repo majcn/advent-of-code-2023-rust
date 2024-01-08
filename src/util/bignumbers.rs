13c2bf7c528129cd306bbf53a07be019d5742db4
Gregor Majcen <majcn.m@gmail.com>
2024-01-08 18:06:02 +0100

    refactor bignumbers => U1024 to UX64<N>

--- src/util/bignumbers.rs
+++ src/util/bignumbers.rs
@@ -1,27 +1,31 @@
 #[derive(Debug, Clone, Copy)]
-pub struct U1024([u64; 16]);
+pub struct UX64<const N: usize>([u64; N]);
 
-impl U1024 {
+impl<const N: usize> UX64<N> {
     pub fn count_ones(&self) -> u32 {
         self.0.iter().map(|x| x.count_ones()).sum()
     }
 
-    pub const ZERO: U1024 = U1024([0; 16]);
-    pub const ONE: U1024 = U1024([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    pub const ZERO: UX64<N> = UX64([0; N]);
+    pub const ONE: UX64<N> = {
+        let mut result = UX64([0; N]);
+        result.0[0] = 1;
+        result
+    };
 }
 
-impl std::ops::Shl<usize> for U1024 {
-    type Output = U1024;
+impl<const N: usize> std::ops::Shl<usize> for UX64<N> {
+    type Output = UX64<N>;
 
     fn shl(self, rhs: usize) -> Self::Output {
-        let mut result = U1024::ZERO;
+        let mut result = UX64::ZERO;
         let word_shift = rhs / 64;
         let bit_shift = rhs % 64;
-        for i in 0..16 {
-            if bit_shift < 64 && i + word_shift < 16 {
+        for i in 0..N {
+            if bit_shift < 64 && i + word_shift < N {
                 result.0[i + word_shift] += self.0[i] << bit_shift;
             }
-            if bit_shift > 0 && i + word_shift + 1 < 16 {
+            if bit_shift > 0 && i + word_shift + 1 < N {
                 result.0[i + word_shift + 1] += self.0[i] >> (64 - bit_shift);
             }
         }
@@ -29,16 +33,16 @@ impl std::ops::Shl<usize> for U1024 {
     }
 }
 
-impl std::ops::Shr<usize> for U1024 {
-    type Output = U1024;
+impl<const N: usize> std::ops::Shr<usize> for UX64<N> {
+    type Output = UX64<N>;
 
-    fn shr(self, shift: usize) -> U1024 {
-        let mut result = U1024::ZERO;
+    fn shr(self, shift: usize) -> Self::Output {
+        let mut result = UX64::ZERO;
         let word_shift = shift / 64;
         let bit_shift = shift % 64;
-        for i in word_shift..16 {
+        for i in word_shift..N {
             result.0[i - word_shift] += self.0[i] >> bit_shift;
-            if bit_shift > 0 && i < 16 - 1 {
+            if bit_shift > 0 && i < N - 1 {
                 result.0[i - word_shift] += self.0[i + 1] << (64 - bit_shift);
             }
         }
@@ -46,44 +50,44 @@ impl std::ops::Shr<usize> for U1024 {
     }
 }
 
-impl std::ops::BitAnd for U1024 {
-    type Output = U1024;
+impl<const N: usize> std::ops::BitAnd for UX64<N> {
+    type Output = UX64<N>;
 
     fn bitand(self, rhs: Self) -> Self::Output {
-        let mut result = U1024::ZERO;
-        for i in 0..16 {
+        let mut result = UX64::ZERO;
+        for i in 0..N {
             result.0[i] = self.0[i] & rhs.0[i];
         }
         result
     }
 }
 
-impl std::ops::BitOr for U1024 {
-    type Output = U1024;
+impl<const N: usize> std::ops::BitOr for UX64<N> {
+    type Output = UX64<N>;
 
     fn bitor(self, rhs: Self) -> Self::Output {
-        let mut result = U1024::ZERO;
-        for i in 0..16 {
+        let mut result = UX64::ZERO;
+        for i in 0..N {
             result.0[i] = self.0[i] | rhs.0[i];
         }
         result
     }
 }
 
-impl std::ops::BitOrAssign for U1024 {
+impl<const N: usize> std::ops::BitOrAssign for UX64<N> {
     fn bitor_assign(&mut self, rhs: Self) {
-        for i in 0..16 {
+        for i in 0..N {
             self.0[i] = self.0[i] | rhs.0[i];
         }
     }
 }
 
-impl std::ops::Not for U1024 {
-    type Output = U1024;
+impl<const N: usize> std::ops::Not for UX64<N> {
+    type Output = UX64<N>;
 
     fn not(self) -> Self::Output {
-        let mut result = U1024::ZERO;
-        for i in 0..16 {
+        let mut result = UX64::ZERO;
+        for i in 0..N {
             result.0[i] = !self.0[i];
         }
         result
